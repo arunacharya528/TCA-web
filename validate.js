8fb911de448358deec21f02006f91511e8cc5b87
RED BEAST <arunacharya528>
2019-05-09 11:01:06 +0545

    finished all editings

---
+++ validate.js
@@ -0,0 +1,14 @@
+function prompt(){
+    var name = document.getElementById("name").value;
+    //alert(name);
+    var email = document.getElementById("email").value;
+    //alert(email);
+    if(name == ''){
+        document.getElementById("remarks").innerHTML
+        ="Enter a name so that we may know you by something";
+    }
+    else if (email == ''){
+        document.getElementById("remarks").innerHTML
+        ="Enter your Email for contacting you"
+    }
+}
